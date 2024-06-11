a6506ccb067051aef44c9f90b47c6872410cee4e
Vincent Jue <viny1976@gmail.com>
2024-06-11 10:50:08 +0000

    page accueil modifié

--- index.js
+++ index.js
@@ -1,66 +1,123 @@
-const selectAgents = document.querySelector("#agent-select");
+const selectAgents = document.getElementById('agent-select')
+const horaire = document.getElementById('horaire')
+const dateDepart = document.getElementById('dateDepart')
+const natureContrat = document.getElementById('natureContrat')
+const codeService = document.getElementById('codeService')
+const creation = document.getElementById('creation')
+const codeRemuneration = document.getElementById('codeRemuneration')
+const nom = document.getElementById('nom')
+const prenom = document.getElementById('prenom')
 
-console.log("document", document.getElementById("agent-select"));
+const form = document.querySelector('form')
+const yearSelect = document.querySelector('#year-select')
+const submitBtn = document.querySelector('#submit-btn')
 
-fetch("../data/bd-2024.json")
+fetch('../data/bd.json')
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
+    console.log(data)
 
-    // Extract agents and modifications
-    const modifications = {};
+    submitBtn.addEventListener('click', (e) => {
+      e.preventDefault() // prevent page reload
 
-    // Extract unique agents
-    const agents = [
-      ...new Map(
-        Object.values(data).flatMap((date) =>
-          date.map((agent) => [agent.numAgent, agent])
-        )
-      ).values(),
-    ];
+      const selectedYear = yearSelect.value
 
-    // Sort agents by numAgent
-    agents.sort((a, b) => a.numAgent - b.numAgent);
+      console.log(`Selected year: ${selectedYear}`)
 
-    console.log("agents", agents);
+      let values = Object.values(data)
 
-    // Remove the existing options
-    selectAgents.innerHTML = "";
+      const filteredData = values.map((val) => {
+        return val.filter((v) => v.dateSynchronisation.split('-')[0] === selectedYear)
+      
+      })
 
-    // Add new options to the select element
-    agents.forEach((agent) => {
-      const option = document.createElement("option");
-      option.value = agent.agent;
-      option.text = agent.agent;
-      selectAgents.appendChild(option);
-    });
+      // Extract unique agents
+      const agents = [
+        ...new Map(
+          Object.values(filteredData).flatMap((date) =>
+            date.map((agent) => [agent.numAgent, agent]),
+          ),
+        ).values(),
+      ]
 
-    selectAgents.addEventListener("change", (e) => {
-      const selectedAgent = e.target.value;
-      // Do something with the selected agent, e.g. filter the modifications list
-      console.log(`Selected agent: ${selectedAgent}`);
-    });
+      // Sort agents by numAgent
+      agents.sort((a, b) => a.numAgent - b.numAgent)
 
-    Object.keys(data).forEach((dateKey) => {
-      data[dateKey].forEach((agent) => {
-        agent.lignes.forEach((modification) => {
-          modifications[modification.modification] =
-            (modifications[modification.modification] || 0) + 1;
-        });
-      });
-    });
+      console.log('agents', agents)
 
-    // Render modifications list
-    const modificationList = document.getElementById("modification-list");
-    Object.keys(modifications).forEach((modification) => {
-      const modificationLi = document.createElement("li");
-      modificationLi.textContent = `${
-        modification === "" ? "CREATION" : modification
-      } (${modifications[modification]})`;
-      modificationLi.className = "modification";
-      modificationList.appendChild(modificationLi);
-    });
+      if (selectAgents) {
+        // Remove the existing options
+        selectAgents.innerHTML = ''
+
+        // Add new options to the select element
+        agents.forEach((agent) => {
+          const option = document.createElement('option')
+          option.value = agent.agent
+          option.text = agent.agent
+          selectAgents.appendChild(option)
+        })
+
+        selectAgents.addEventListener('change', (e) => {
+          const selectedAgent = e.target.value
+          // Do something with the selected agent, e.g. filter the modifications list
+          console.log(`Selected agent: ${selectedAgent}`)
+        })
+      }
+
+      // Extract agents and modifications
+      const modifications = {}
+
+      if (!selectAgents) {
+        Object.keys(filteredData).forEach((dateKey) => {
+          filteredData[dateKey].forEach((agent) => {
+            agent.lignes.forEach((modification) => {
+              modifications[modification.modification] =
+                (modifications[modification.modification] || 0) + 1
+            })
+          })
+        })
+
+        console.log('modifications', modifications)
+
+        // Render modifications
+
+        Object.keys(modifications).forEach((modification) => {
+          console.log('modification', modification)
+          if (modification == 'HORAIREHEBDOMADAIRE') {
+            horaire.innerText = `${modifications[modification]}`
+          }
+          if (modification == 'CODEREMUNERATION') {
+            console.log('modification', modification)
+            codeRemuneration.innerText = `${modifications[modification]}`
+          }
+          if (modification == 'CODESERVICE') {
+            console.log('modification', modification)
+            codeService.innerText = `${modifications[modification]}`
+          }
+          if (modification == 'DATEDEPART') {
+            console.log('modification', modification)
+            dateDepart.innerText = `${modifications[modification]}`
+          }
+          if (modification == 'NATURECONTRAT') {
+            console.log('modification', modification)
+            natureContrat.innerText = `${modifications[modification]}`
+          }
+          if (modification == '') {
+            console.log('modification', modification)
+            creation.innerText = `${modifications[modification]}`
+          }
+          if (modification == 'NOM') {
+            console.log('modification', modification)
+            nom.innerText = `${modifications[modification]}`
+          }
+          if (modification == 'PRENOM') {
+            console.log('modification', modification)
+            prenom.innerText = `${modifications[modification]}`
+          }
+        })
+      }
+    })
   })
   .catch((error) => {
-    console.error("Error:", error);
-  });
+    console.error('Error:', error)
+  })
